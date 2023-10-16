5ad38ee323ebd23ae9dd263f48e5a84fe7c95df0
Jairiz Edward Esposo <jesposo@awesomeos.com>
2023-10-17 02:16:40 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { BlogComponent } from './components/pages/blog/blog.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { CartComponent } from './components/pages/cart/cart.component';
+import { ProductDetailsComponent } from './components/pages/product-details/product-details.component';
+import { ProductCheckoutComponent } from './components/pages/product-checkout/product-checkout.component';
+import { NavBarComponent } from './components/layout-components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/layout-components/footer/footer.component';
+import { SubNavBarComponent } from './components/layout-components/sub-nav-bar/sub-nav-bar.component';
+import { PromoComponent } from './components/layout-components/promo/promo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    ContactComponent,
+    CartComponent,
+    ProductDetailsComponent,
+    ProductCheckoutComponent,
+    NavBarComponent,
+    FooterComponent,
+    SubNavBarComponent,
+    PromoComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/pages/product-checkout/product-checkout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-checkout',
+  templateUrl: './product-checkout.component.html',
+  styleUrls: ['./product-checkout.component.scss']
+})
+export class ProductCheckoutComponent {
+
+}
